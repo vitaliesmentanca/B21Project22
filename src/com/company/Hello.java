8089e7081c51d55e75148215b13763f3d6de7ad6
vitaliesmentanca <56801290+vitaliesmentanca@users.noreply.github.com>
2020-11-10 14:38:15 -0600

    addind some lines

--- src/com/company/Hello.java
+++ src/com/company/Hello.java
@@ -3,6 +3,7 @@ package com.company;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello again");
+        System.out.println("added line 6");
 
     }
 }
