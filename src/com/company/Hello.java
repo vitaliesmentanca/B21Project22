9d6664109aaeff49b4d813d727d31705ed4aa9a7
vitaliesmentanca <56801290+vitaliesmentanca@users.noreply.github.com>
2020-11-10 14:41:27 -0600

    adding class

--- src/com/company/Hello.java
+++ src/com/company/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello again");
         System.out.println("added line 6");
+        System.out.println("added line 7");
+        System.out.println("added line 8");
 
     }
 }
